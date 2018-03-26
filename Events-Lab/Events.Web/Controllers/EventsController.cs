d6b02d01da7441e06af4fc772b6ebece42f001ae
Mark Riley <markjriley1899@gmail.com>
2018-03-26 15:48:46 +0100

    Work up until section 11

---
+++ Events-Lab/Events.Web/Controllers/EventsController.cs
@@ -0,0 +1,50 @@
+﻿using Events.Data;
+using Events.Web.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Events.Web.Controllers
+{
+    public class EventsController : BaseController
+    {
+        public ActionResult My()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EventInputModel model)
+        {
+            if (model != null && this.ModelState.IsValid)
+            {
+                var e = new Event()
+                {
+                    AuthorId = this.User.Identity.GetUserId(),
+                    Title = model.Title,
+                    StartDateTime = model.StartDateTime,
+                    Duration = model.Duration,
+                    Description = model.Description,
+                    Location = model.Location,
+                    IsPublic = model.isPublic
+                };
+                this.db.Events.Add(e);
+                this.db.SaveChanges();
+               // this.AddNotification("Event created.", NotificationType.INFO);
+                return this.RedirectToAction("My");
+            }
+
+            return this.View(model);
+        }
+    }
+}
--- Events-Lab/Events.Web/Controllers/HomeController.cs
+++ Events-Lab/Events.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Events.Web.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,13 +8,40 @@ using System.Web.Mvc;
 
 namespace Events.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
-            return View();
+            var events = this.db.Events
+                .OrderBy(e => e.StartDateTime)
+                .Where(e => e.IsPublic)
+                .Select(EventViewModel.ViewModel);
+
+            var upcomingEvents = events.Where(e => e.StartDateTime > DateTime.Now);
+            var passedEvents = events.Where(e => e.StartDateTime <= DateTime.Now);
+            return View(new UpcomingPassedEventsViewModel()
+            {
+                UpcomingEvents = upcomingEvents,
+                PassedEvents = passedEvents
+            });
         }
 
-      
+        public ActionResult EventDetailsById(int id)
+        {
+            var currentUserId = this.User.Identity.GetUserId();
+            var isAdmin = this.isAdmin();
+            var eventDetails = this.db.Events
+                .Where(e => e.Id == id)
+                .Where(e => e.IsPublic || isAdmin || (e.AuthorId != null && e.AuthorId == currentUserId))
+                .Select(EventDetailsViewModel.ViewModel)
+                .FirstOrDefault();
+
+            var isOwner = (eventDetails != null && eventDetails.AuthorId != null &&
+                eventDetails.AuthorId == currentUserId);
+            this.ViewBag.CanEdit = isOwner || isAdmin;
+
+            return this.PartialView("_EventDetails", eventDetails);
+        }
+
     }
 }
